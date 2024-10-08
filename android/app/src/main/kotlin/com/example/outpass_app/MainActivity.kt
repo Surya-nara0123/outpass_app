83c70e3568eaa24bf6d238a80a323d59b89b8109
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-10-08 23:48:40 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/outpass_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.outpass_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+/// The Widget that configures your application.
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
