c5ee88a79f68aec274869fdcaf3b21500aebad1e
Senthan <senthanbalu.com>
2024-10-12 05:32:05 +0000

    Removed backend folder

--- backend/Models/AuthorityModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const AuthorSchema = new mongoose.Schema({
-    Name: String,   // Match the field name exactly as it appears in MongoDB
-    Mail: String,
-});
-
-const AuthorModel = mongoose.model("authors", AuthorSchema);
-module.exports = AuthorModel;
