798ead0750cfb0c42ad4043dda7a808621cc3244
Surya Narayanan <surya23110035@snuchennai.edu.in>
2024-10-12 16:12:37 +0530

    integrated the home page ui and the outpass apply page ui

--- lib/src/tabs/home.dart
+++ lib/src/tabs/home.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:outpass_app/src/components/appbar.dart';
 import 'package:outpass_app/src/tabs/apply_outpass.dart';
+import 'package:outpass_app/src/tabs/apply_outpass_emergency.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +11,139 @@ class Home extends StatefulWidget {
   @override
   State<Home> createState() => _HomeState();
 }
+
+void _showPopupMenu(BuildContext context, Offset tapPosition) async {
+    final RenderBox overlay = Overlay.of(context).context.findRenderObject() as RenderBox;
+
+    await showMenu(
+      context: context,
+      position: RelativeRect.fromRect(
+        tapPosition & const Size(40, 40), // Popup position size
+        Offset.zero & overlay.size, // Screen size
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8.0),
+      ),
+      menuPadding: const EdgeInsets.all(10.0),
+      items: [
+        PopupMenuItem(
+          padding: const EdgeInsets.only(bottom: 10.0),
+          labelTextStyle: WidgetStatePropertyAll(
+            TextStyle(color: Colors.black),
+            ),
+          value: 1,
+          child: Container(
+            height: 50.0,
+            decoration: BoxDecoration(
+              color: const Color(0xFFD0BCFF),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Center(child: Text('Emergency Outpass'))
+            ),
+        ),
+        PopupMenuItem(
+          padding: const EdgeInsets.only(bottom: 10.0),
+          labelTextStyle: WidgetStatePropertyAll(
+            TextStyle(color: Colors.black),
+            ),
+          value: 2,
+          child: Container(
+            height: 50.0,
+            decoration: BoxDecoration(
+              color: const Color(0xFFD0BCFF),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Center(child: Text('Regular Timing Outpass'))
+            ),
+        ),
+        PopupMenuItem(
+          padding: const EdgeInsets.only(bottom: 00.0),
+          labelTextStyle: WidgetStatePropertyAll(
+            TextStyle(color: Colors.black),
+            ),
+          value: 2,
+          child: Container(
+            height: 50.0,
+            decoration: BoxDecoration(
+              color: const Color(0xFFD0BCFF),
+              borderRadius: BorderRadius.circular(16.0),
+            ),
+            child: Center(child: Text('Special Timing Outpass'))
+            ),
+        ),
+      ],
+    ).then((value) {
+      if (value != null) {
+        // Handle selection of options
+        if (value == 1) {
+          context.mounted ?
+          Navigator.of(context).push(
+            PageRouteBuilder(
+              pageBuilder: (context, animation, secondaryAnimation) =>
+                  const ApplyOutpassEmergency(),
+              transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                const begin = Offset(1.0, 0.0); // Start from the right
+                const end = Offset.zero;
+                const curve = Curves.ease;
+
+                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                var offsetAnimation = animation.drive(tween);
+
+                return SlideTransition(
+                  position: offsetAnimation,
+                  child: child,
+                );
+              },
+            ),
+          ):null;
+        }
+        if (value == 2) {
+          context.mounted ?
+          Navigator.of(context).push(
+            PageRouteBuilder(
+              pageBuilder: (context, animation, secondaryAnimation) =>
+                  const ApplyOutpass(),
+              transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                const begin = Offset(1.0, 0.0); // Start from the right
+                const end = Offset.zero;
+                const curve = Curves.ease;
+
+                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                var offsetAnimation = animation.drive(tween);
+
+                return SlideTransition(
+                  position: offsetAnimation,
+                  child: child,
+                );
+              },
+            ),
+          ):null;
+        }
+        if (value == 3) {
+          context.mounted ?
+          Navigator.of(context).push(
+            PageRouteBuilder(
+              pageBuilder: (context, animation, secondaryAnimation) =>
+                  const ApplyOutpass(),
+              transitionsBuilder: (context, animation, secondaryAnimation, child) {
+                const begin = Offset(1.0, 0.0); // Start from the right
+                const end = Offset.zero;
+                const curve = Curves.ease;
+
+                var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+                var offsetAnimation = animation.drive(tween);
+
+                return SlideTransition(
+                  position: offsetAnimation,
+                  child: child,
+                );
+              },
+            ),
+          ):null;
+        }
+      }
+    });
+  }
 
 class _HomeState extends State<Home> {
   @override
@@ -48,30 +182,32 @@ class _HomeState extends State<Home> {
                         color: const Color(0xFFD0BCFF),
                         child: InkWell(
                           borderRadius: BorderRadius.circular(16.0),
+                          onTapUp: (details) => 
+                            _showPopupMenu(context, details.globalPosition),
                           onTap: () {
-                            Navigator.of(context).push(
-                              PageRouteBuilder(
-                                pageBuilder:
-                                    (context, animation, secondaryAnimation) =>
-                                        const ApplyOutpass(),
-                                transitionsBuilder: (context, animation,
-                                    secondaryAnimation, child) {
-                                  const begin =
-                                      Offset(1.0, 0.0); // Start from the right
-                                  const end = Offset.zero;
-                                  const curve = Curves.ease;
+                            // Navigator.of(context).push(
+                            //   PageRouteBuilder(
+                            //     pageBuilder:
+                            //         (context, animation, secondaryAnimation) =>
+                            //             const ApplyOutpass(),
+                            //     transitionsBuilder: (context, animation,
+                            //         secondaryAnimation, child) {
+                            //       const begin =
+                            //           Offset(1.0, 0.0); // Start from the right
+                            //       const end = Offset.zero;
+                            //       const curve = Curves.ease;
 
-                                  var tween = Tween(begin: begin, end: end)
-                                      .chain(CurveTween(curve: curve));
-                                  var offsetAnimation = animation.drive(tween);
+                            //       var tween = Tween(begin: begin, end: end)
+                            //           .chain(CurveTween(curve: curve));
+                            //       var offsetAnimation = animation.drive(tween);
 
-                                  return SlideTransition(
-                                    position: offsetAnimation,
-                                    child: child,
-                                  );
-                                },
-                              ),
-                            );
+                            //       return SlideTransition(
+                            //         position: offsetAnimation,
+                            //         child: child,
+                            //       );
+                            //     },
+                            //   ),
+                            // );
                           },
                           splashColor: const Color(0xFFBFABEE).withOpacity(1.0),
                           child: const Column(
