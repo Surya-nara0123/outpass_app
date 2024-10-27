50178c12292ced10d16f213b0c198c50c4e428b9
Sharan <sharweshraajan@gmail.com>
2024-10-27 12:42:25 +0530

    updated navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,28 +1,65 @@
-import './nav.css'
+import React, { useState } from 'react';
+import './nav.css';
 import logo from '../assets/logo.png';
-import { RiNotification2Line} from 'react-icons/ri';
-import {FaCaretDown, FaUserCircle} from 'react-icons/fa';
-export function Navbar({islogged}) {
-    if (islogged===1){
+import { FaCaretDown, FaCaretUp, FaUserCircle, FaBell, FaSignOutAlt } from 'react-icons/fa';
+
+export function Navbar({ islogged }) {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+    };
+
+    const handleLogout = () => {
+        // Logic to log out the user
+        console.log('Logging out...');
+        setDropdownOpen(false); // Close the dropdown on logout
+    };
+
+    const handleNotifications = () => {
+        // Logic to show notifications
+        console.log('Showing notifications...');
+        setDropdownOpen(false); // Close the dropdown after clicking
+    };
+
+    const handleProfile = () => {
+        // Logic to navigate to the profile page
+        console.log('Navigating to profile...');
+        setDropdownOpen(false); // Close the dropdown after clicking
+    };
+
+    if (islogged === 1) {
         return (
             <div className='Navbar'>
-              <div className='header'>
-                <img src={logo} alt="Logo" className='logo' />
-                Outpass App
-              </div>
-              <div className='right-push'>
-                <div className='user-info'>
-                  <FaUserCircle className='icon' />
+                <div className='header'>
+                    <img src={logo} alt="Logo" className='logo' />
+                    Outpass App
                 </div>
-              </div>
+                <div className='right-push'>
+                    <div className='user-info' onClick={toggleDropdown}>
+                        <FaUserCircle className='icon' />
+                        {dropdownOpen ? (
+                            <FaCaretUp className='icon' style={{ marginLeft: '0px', marginRight: '10px' }} />
+                        ) : (
+                            <FaCaretDown className='icon' style={{ marginLeft: '0px', marginRight: '10px' }} />
+                        )}
+                    </div>
+                    {dropdownOpen && (
+                        <div className='dropdown-menu'>
+                            <button onClick={handleNotifications}>
+                                <FaBell style={{ marginRight: '8px' }} />
+                                Notifications
+                            </button>
+                            <button onClick={handleLogout}>
+                                <FaSignOutAlt style={{ marginRight: '8px' }} />
+                                Logout
+                            </button>
+                        </div>
+                    )}
+                </div>
             </div>
-          );
+        );
+    } else {
+        return <div></div>;
     }
-    else{
-        return(
-            <div>
-            </div>
-        )
-    }
-
 }
